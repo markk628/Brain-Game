e7c837c0aee4ce2f76532d08a031c89940253027
Donghyun <markk628@gmail.com>
2020-03-05 15:29:48 -0800

    test

--- BrainGame/FirstViewController.swift
+++ BrainGame/FirstViewController.swift
@@ -14,6 +14,8 @@ class FirstVc: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    
     @IBAction func goBackToVC1(segue:UIStoryboardSegue){
     }
     
